fa985e90046617072bfde7c96481959825b3462f
brayan.quirino <brayan.quirino@bbva.com>
2022-11-03 16:29:28 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+const name = 'Hi I am Brayan'
+
+export const App = () => {
+    return (
+    <>
+        <h1>{name}</h1>
+        <p>Soy un subtitulo</p>
+    </>
+    );
+    
+}
+
+/*export const App = () => <h1>Hola Mundo</h1>;*/
